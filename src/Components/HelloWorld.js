8358f1335d0a63b9019739c59432bf1fde712df8
Karthikkuddigana <karthikkuddigana@gmail.com>
2023-10-22 13:00:46 +0530

    New Component HelloWorld added

---
+++ src/Components/HelloWorld.js
@@ -0,0 +1,14 @@
+import React from 'react'; 
+
+function HelloWorld(){
+    return(
+        <>
+            <div className='footer'>
+                <p>Hello World</p>
+                <p background-color='blue'>This is a react app</p>
+            </div>
+        </>
+        
+    )
+}
+export default Footer; 
